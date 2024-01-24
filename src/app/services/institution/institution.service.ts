fbbf4aee79f8841950969e87b829b6f8d085f9f1
Ruben <r.c.a2312@gmail.com>
2024-01-24 13:43:13 +0100

    add method getAll and getById

--- src/app/services/institution/institution.service.ts
+++ src/app/services/institution/institution.service.ts
@@ -1,15 +1,16 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InstitutionService {
-  private http = inject(HttpClient);
+  private readonly http = inject(HttpClient);
 
-  public async getAll(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      this.http.get('http://localhost:3000/institutions')
+  public async getAll (): Promise<any> {
+    return await new Promise((resolve, reject) => {
+      this.http.get(environment.api.url + environment.api.institutions + '/page')
         .subscribe({
           next: (data) => {
             resolve(data);
@@ -17,7 +18,21 @@ export class InstitutionService {
           error: (error) => {
             reject(error);
           }
-        }); 
+        });
+    });
+  }
+
+  public async getInstitutionById (id: string): Promise<any> {
+    return await new Promise((resolve, reject) => {
+      this.http.get(environment.api.url + environment.api.institutions + id)
+        .subscribe({
+          next: (data) => {
+            resolve(data);
+          },
+          error: (error) => {
+            reject(error);
+          }
+        });
     });
   }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,9 @@
-export const environment = {};
+export const environment = {
+    production: false,
+    api: {
+        url: 'http://localhost:8080/',
+        institutions: 'institution/',
+        users: 'institution/users/',
+        schoolYear: 'institution/school-year/',
+    }
+};
