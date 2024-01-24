850f0275995404e8b0609e58b194acb13086c158
najocar <josecarloscastrolopez30@gmail.com>
2024-01-24 13:24:46 +0100

    JCC update sidebar

--- src/app/pages/jccpage/jccpage.component.ts
+++ src/app/pages/jccpage/jccpage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-jccpage',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent],
   templateUrl: './jccpage.component.html',
   styleUrl: './jccpage.component.scss'
 })
