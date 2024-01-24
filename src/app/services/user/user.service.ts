9ed5024f7275617868970d9cbacd0efa800db9e9
Elcheno <r.c.a2312@gmail.com>
2024-01-24 23:06:32 +0100

    RCA implement methods in user service

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment as env } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private readonly http = inject(HttpClient);
 
-  constructor() { }
+  public async getAllMock (): Promise<any> {
+    return await new Promise((resolve, _reject) => {
+      resolve({ data: [] });
+    });
+  }
+
+  public async getAll (): Promise<any> {
+    return await new Promise((resolve, reject) => {
+      this.http.get(env.api.url + env.api.users + '/page')
+        .subscribe({
+          next: (data) => {
+            resolve(data);
+          },
+          error: (error) => {
+            reject(error);
+          }
+        });
+    });
+  }
+
+  public async getById (id: string): Promise<any> {
+    return await new Promise((resolve, reject) => {
+      this.http.get(env.api.url + env.api.users + id)
+        .subscribe({
+          next: (data) => {
+            resolve(data);
+          },
+          error: (error) => {
+            reject(error);
+          }
+        });
+    });
+  }
 }
