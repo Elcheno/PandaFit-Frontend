0782da8bee3241ce06880d7cfba0c78d15311fe8
Elcheno <r.c.a2312@gmail.com>
2024-01-24 16:03:59 +0100

    RCA add methods login and logout, and implement signal with auth info

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,54 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  public sessionData = signal(null);
+
+  constructor() {}
+
+  public setSessionData(data: any) {
+    this.sessionData.set(data);
+  }
+
+  public async login(): Promise<any> {
+    return await new Promise((resolve, reject) => {
+
+      setTimeout(() => {
+        try {
+          const user = {
+            email: 'usuario@example.com',
+            password: 'usuario'
+          }
+          this.setSessionData(user);
+          window.localStorage.setItem('sessionData', JSON.stringify(user));
+          resolve(user);
+          
+        } catch (error) {
+          reject(error);
+
+        }
+      }, 1000);
+    });
+  }
+
+  public async logOut(): Promise<void> {
+    return await new Promise((resolve, reject) => {
+
+      setTimeout(() => {
+        try {
+          this.setSessionData(null);
+          window.localStorage.removeItem('sessionData');
+          resolve();
+
+        } catch (error) {
+          reject(error);
+
+        }
+      }, 1000);
+    });
+  } 
+
 }
