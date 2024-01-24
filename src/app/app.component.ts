7dfd2fa18cb2d40de695e77b55b2b299f9c48c1c
Ruben <r.c.a2312@gmail.com>
2024-01-24 12:09:34 +0100

    re first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit {
+  title = 'Pandafit-Frontend';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+}
