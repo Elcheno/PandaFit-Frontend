bbb48ab1d9338cd08945e08f69e4e5115c910b18
Elcheno <r.c.a2312@gmail.com>
2024-01-24 22:59:55 +0100

    RCA implement methods in schoolyear service

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,9 @@
 export const environment = {
-    production: false,
-    api: {
-        url: 'http://localhost:8080/',
-        institutions: 'institution/',
-        users: 'institution/user/',
-        schoolYear: 'institution/school-year/',
-    }
+  production: false,
+  api: {
+    url: 'http://localhost:8080/',
+    institutions: 'institution/',
+    users: 'institution/user/',
+    schoolyear: 'institution/school-year/'
+  }
 };
