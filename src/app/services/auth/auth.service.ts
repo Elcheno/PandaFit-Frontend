c1f1677d7308e9b2c77c0503fcb35b3d6ac478e6
Elcheno <r.c.a2312@gmail.com>
2024-01-24 17:16:12 +0100

    RCA add methods in AuthService

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -4,51 +4,41 @@ import { Injectable, signal } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
+  public sessionData = signal<any>(null);
 
-  public sessionData = signal(null);
-
-  constructor() {}
-
-  public setSessionData(data: any) {
+  public setSessionData (data: any): void {
     this.sessionData.set(data);
   }
 
-  public async login(): Promise<any> {
+  public async login (): Promise<any> {
     return await new Promise((resolve, reject) => {
-
       setTimeout(() => {
         try {
           const user = {
             email: 'usuario@example.com',
             password: 'usuario'
-          }
+          };
           this.setSessionData(user);
           window.localStorage.setItem('sessionData', JSON.stringify(user));
           resolve(user);
-          
         } catch (error) {
           reject(error);
-
         }
       }, 1000);
     });
   }
 
-  public async logOut(): Promise<void> {
-    return await new Promise((resolve, reject) => {
-
+  public async logOut (): Promise<void> {
+    await new Promise<void>((resolve, reject) => {
       setTimeout(() => {
         try {
           this.setSessionData(null);
           window.localStorage.removeItem('sessionData');
           resolve();
-
         } catch (error) {
           reject(error);
-
         }
       }, 1000);
     });
-  } 
-
+  }
 }
