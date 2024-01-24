dc0bc142f536690163bb0d217298929fea36cbc5
najocar <josecarloscastrolopez30@gmail.com>
2024-01-24 12:23:08 +0100

    JCC createPages

---
+++ src/app/pages/jccpage/jccpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jccpage',
+  standalone: true,
+  imports: [],
+  templateUrl: './jccpage.component.html',
+  styleUrl: './jccpage.component.scss'
+})
+export class JCCPageComponent {
+
+}
