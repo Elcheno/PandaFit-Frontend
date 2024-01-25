add66146af587eb42b7fad3978a5a17d41bbf13b
najocar <josecarloscastrolopez30@gmail.com>
2024-01-25 18:38:28 +0100

    JCC Update Institutios page

--- src/app/pages/institutions/institutions.component.ts
+++ src/app/pages/institutions/institutions.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TableInstitutionComponent } from '../../components/table-institution/table-institution.component';
+import { InstitutionService } from '../../services/institution/institution.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { IInstitution } from '../../model/interfaces/i-institution';
 
 @Component({
   selector: 'app-institutions',
   standalone: true,
-  imports: [TableInstitutionComponent],
+  imports: [TableInstitutionComponent, ReactiveFormsModule],
   templateUrl: './institutions.component.html',
   styleUrl: './institutions.component.scss'
 })
 export class InstitutionsComponent {
+  public form!:FormGroup;
+  private institutionService = inject(InstitutionService)
+
+  constructor(private fb:FormBuilder){
+    this.form = this.fb.group({
+      name: ''
+    })
+  }
+  submit() {
+    let newInstitution:IInstitution = {
+      id: '',
+      name: this.form.value.name,
+      userList: []
+    }
+    // alert('SUCCESS!! :-)\n\n' + JSON.stringify(newInstitution))
+    this.institutionService.create(newInstitution)
+    this.form.reset();
+  }
 
 }
