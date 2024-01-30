00cc7289c9b01d4c07f3417940f750af5175dfa8
Elcheno <r.c.a2312@gmail.com>
2024-01-30 20:33:28 +0100

    RCA update styles and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
-    "./node_modules/flowbite/**/*.js"
+    './src/**/*.{html,ts}',
+    './node_modules/flowbite/**/*.js'
   ],
   theme: {
     extend: {
       colors: {
         'green-primary': '#57AD57',
-        'black-primary': '#000000'
+        'green-dark': '#2C5A2E',
+        'green-light': '#9ED79F',
+        'black-primary': '#27272A',
+        'black-secondary': '#3F3F46'
       }
-    },
+    }
   },
   plugins: [
     require('flowbite/plugin')
-  ],
-}
-
+  ]
+};
