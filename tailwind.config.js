ed9ba73ee22e6e1eab9c6314619d2f8983f4f8c4
Elcheno <r.c.a2312@gmail.com>
2024-01-30 21:06:02 +0100

    RCA update styles create user modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
       colors: {
         'green-primary': '#57AD57',
         'green-dark': '#2C5A2E',
-        'green-light': '#9ED79F',
+        'green-light': '#6ABB6B',
         'black-primary': '#27272A',
         'black-secondary': '#3F3F46'
       }
