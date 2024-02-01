8639336191c391c860aca31a3077268444570fbe
Elcheno <r.c.a2312@gmail.com>
2024-02-01 21:01:44 +0100

    RCA create dropdown generic in progress

--- src/app/components/users/table-users/table-users.component.ts
+++ src/app/components/users/table-users/table-users.component.ts
@@ -1,14 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/array-type */
 import { Component, Input, Output, type OnInit, EventEmitter, inject } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { type IUser } from '../../../model/interfaces/i-user';
 import { type IPageable } from '../../../model/interfaces/i-pageable';
 import { CdkMenu, CdkMenuItem, CdkMenuTrigger } from '@angular/cdk/menu';
 import { ModalConfirmService } from '../../../services/modal/modal-confirm.service';
+import { DropdownComponent } from '../../dropdown/dropdown.component';
+import { type IDropdownData } from '../../../model/interfaces/i-dropdown';
 
 @Component({
   selector: 'app-table-users',
   standalone: true,
-  imports: [CdkMenuTrigger, CdkMenu, CdkMenuItem, NgFor],
+  imports: [CdkMenuTrigger, CdkMenu, CdkMenuItem, NgFor, DropdownComponent],
   templateUrl: './table-users.component.html',
   styleUrl: './table-users.component.scss'
 })
@@ -29,19 +33,34 @@ export class TableUsersComponent implements OnInit {
     content: []
   };
 
+  public dropdownRows: IDropdownData<IUser> = {
+    button: {
+      icon: '<svg class="w-[28px] h-[28px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="3" d="M6 12h0m6 0h0m6 0h0" />/svg>'
+    },
+    rows: [
+      {
+        title: 'Update',
+        fnc: (data: any) => {
+          if (data == null) return;
+          console.log(data);
+          this.onUpdate.emit(data);
+        },
+        icon: '<svg class="w-6 h-6 inline mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.3 4.8 2.9 2.9M7 7H4a1 1 0 0 0-1 1v10c0 .6.4 1 1 1h11c.6 0 1-.4 1-1v-4.5m2.4-10a2 2 0 0 1 0 3l-6.8 6.8L8 14l.7-3.6 6.9-6.8a2 2 0 0 1 2.8 0Z" /></svg>'
+      },
+      {
+        title: 'Delete',
+        fnc: async (data: any) => {
+          if (data == null) return;
+          (await (this.confirmService.open('Estas seguro de eliminar este usuario'))).closed.subscribe((res: boolean) => {
+            if (!res) return;
+            this.onDelete.emit(data);
+          });
+        },
+        icon: '<svg class="w-6 h-6 inline mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" /></svg>'
+      }
+    ]
+  };
+
   public async ngOnInit (): Promise<void> {
-  }
-
-  public async delete (user: IUser): Promise<void> {
-    if (user == null) return;
-    (await this.confirmService.open('Estas seguro de eliminar este usuario')).closed.subscribe((res: boolean) => {
-      if (!res) return;
-      this.onDelete.emit(user);
-    });
-  }
-
-  public async update (user: IUser): Promise<void> {
-    if (user == null) return;
-    this.onUpdate.emit(user);
   }
 }
---
+++ src/app/model/interfaces/i-dropdown.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/array-type */
+export interface IDropdownData <T> {
+  button: IDropdownButton
+  rows: IDropdownRow<T>[]
+}
+
+export interface IDropdownButton {
+  title?: string
+  icon?: any
+}
+
+export interface IDropdownRow <T> {
+  title: string
+  fnc?: (data?: T) => void | Promise<void>
+  icon?: any
+}
