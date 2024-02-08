52879d8d0b02c94d902101d1763630764afd01f7
najocar <josecarloscastrolopez30@gmail.com>
2024-02-08 16:30:48 +0100

    JCC Update FormComponent

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -8,7 +8,6 @@ import {
 } from '@angular/cdk/drag-drop';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormArray, FormControl, AbstractControl, ReactiveFormsModule } from '@angular/forms';
-import { FormBuilder, FormGroup, Validators, FormArray, FormControl, AbstractControl, ReactiveFormsModule } from '@angular/forms';
 import { InputService } from '../../services/input/input.service';
 import { IInputData } from '../../model/interfaces/i-input-data';
 import { OutputData } from '../../model/interfaces/i-output-data';
@@ -87,8 +86,7 @@ export class FormComponent {
   }
   onSubmit(){
     //https://stackoverflow.com/questions/40927167/angular-reactiveforms-producing-an-array-of-checkbox-values
-   
-    const form:IFormData = {
+  
     const form:IFormData = {
       id:Math.floor(1000 + Math.random() * 9000)+"",
       name:this.formGroup.get('name')?.value,
