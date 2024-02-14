3410bd98b1abc3fd451b98d7ff9803406a97ddde
Ruben <r.c.a2312@gmail.com>
2024-02-14 12:51:58 +0100

    RCA implement toolbar in formulary

--- src/app/pages/formulary/formulary.component.ts
+++ src/app/pages/formulary/formulary.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-formulary',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLinkActive, RouterLink],
   templateUrl: './formulary.component.html',
   styleUrl: './formulary.component.scss'
 })
