87b34f9335944b0e0c58304ee03dff9c78af55eb
Isabel <isabelroldancordoba@hotmail.com>
2024-02-15 20:03:18 +0100

    IRC Fixed it error text

--- src/app/inputs/table-input/table-input.component.ts
+++ src/app/inputs/table-input/table-input.component.ts
@@ -54,7 +54,7 @@ export class TableInputComponent {
       {
         title: 'Delete',
         icon: '<svg class="w-6 h-6 inline mr-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" /></svg>',
-        fnc: async (data: any) => {
+         fnc: async (data: any) => {
           if (data == null) return;
           (await this.confirmService.open('¿Estas seguro de eliminar este input?')).closed.subscribe((res: boolean) => {
             if (res) this.onDelete.emit(data);
