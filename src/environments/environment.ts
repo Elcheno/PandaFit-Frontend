4a67a503beb7732a7424c57fed56c5e361590ea2
Elcheno <r.c.a2312@gmail.com>
2024-03-06 15:26:14 +0100

    RCA fix https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   dev: false,
   api: {
-    url: 'http://pandafit.us-east-1.elasticbeanstalk.com/',
+    url: 'https://pandafit.us-east-1.elasticbeanstalk.com/',
     institution: 'institution',
     users: '/users',
     schoolyear: '/schoolYear',
