ce4d69cfe8a2500574f66f0d35afe0199a5ef9a3
Elcheno <r.c.a2312@gmail.com>
2024-03-26 16:54:44 +0100

    RCA implement answer page in form active

---
+++ src/app/components/responses/table-responses/table-responses.component.ts
@@ -0,0 +1,91 @@
+import { Component, EventEmitter, Input, Output, ViewChild, inject } from '@angular/core';
+import { IPageable } from '../../../model/interfaces/i-pageable';
+import { DatePipe } from '@angular/common';
+import { IDropdownData } from '../../../model/interfaces/i-dropdown';
+import { ModalConfirmService } from '../../../services/modal/modal-confirm.service';
+import { IPage } from '../../../model/interfaces/i-page';
+import { PaginationComponent } from '../../pagination/pagination.component';
+import { LoaderSpinnerComponent } from '../../loader-spinner/loader-spinner.component';
+import { DropdownComponent } from '../../dropdown/dropdown.component';
+
+@Component({
+  selector: 'app-table-responses',
+  standalone: true,
+  imports: [DatePipe, PaginationComponent, LoaderSpinnerComponent, DropdownComponent],
+  templateUrl: './table-responses.component.html',
+  styleUrl: './table-responses.component.scss'
+})
+export class TableResponsesComponent {
+  @ViewChild('tableLoaderPage') public tableLoader!: any;
+
+  @Input() data!: IPageable<any>;
+
+  @Output() public onChangePage = new EventEmitter<IPage>();
+
+  private readonly confirmService = inject(ModalConfirmService);
+
+  constructor () {}
+
+
+  public dropdownRows: IDropdownData<any> = {
+    header: 'Usuario',
+    button: {
+      icon: '<svg class="w-[28px] h-[28px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="3" d="M6 12h0m6 0h0m6 0h0" />/svg>'
+    },
+    rows: [
+      {
+        title: 'Generar Informe',
+        disabled: true,
+        fnc: (data: any) => {
+          if (data == null) return;
+          
+        },
+        icon: '<svg class="w-6 h-6 inline mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.3 4.8 2.9 2.9M7 7H4a1 1 0 0 0-1 1v10c0 .6.4 1 1 1h11c.6 0 1-.4 1-1v-4.5m2.4-10a2 2 0 0 1 0 3l-6.8 6.8L8 14l.7-3.6 6.9-6.8a2 2 0 0 1 2.8 0Z" /></svg>'
+      },
+      {
+        title: 'Eliminar Respuesta',
+        disabled: true,
+        fnc: async (data: any) => {
+          if (data == null) return;
+          (await (this.confirmService.open('Estas seguro de eliminar este usuario'))).closed.subscribe((res: boolean) => {
+            if (!res) return;
+
+          });
+        },
+        icon: '<svg class="w-6 h-6 inline mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" /></svg>'
+      }
+    ]
+  };
+
+  public nextPage (): void { 
+    if ((this.data.page + 1) > this.data.totalPages) return;
+    this.toggleTableLoader();
+    const page: IPage = {  
+      page: this.data.page + 1,
+      size: this.data.size,
+      sort: this.data.sort 
+    };
+
+    this.onChangePage.emit(page);
+  }
+
+  /**
+   * Navigates to the previous page.
+   */
+  public previousPage (): void {
+    if (this.data.page === 0) return;
+    this.toggleTableLoader();
+    const page: IPage = {  
+      page: this.data.page - 1,
+      size: this.data.size,
+      sort: this.data.sort 
+    };
+
+    this.onChangePage.emit(page);
+  }
+
+  public toggleTableLoader (): void {
+    this.tableLoader.nativeElement.classList.toggle('flex');
+    this.tableLoader.nativeElement.classList.toggle('hidden');
+  }
+}
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: false,
   dev: false,
   api: {
-    url: 'https://pandafit-backend-0-1.onrender.com/',
+    // url: 'https://pandafit-backend-0-1.onrender.com/',
+    url: 'http://localhost:8080/',
     institution: 'institution',
     users: '/users',
     schoolyear: '/schoolYear',
@@ -11,7 +12,8 @@ export const environment = {
     output: '/output',
     formulary: '/formulary',
     login: "login",
-    active: "active"
+    active: "active",
+    response: "response",
   },
   firebaseConfig: {
     apiKey: "AIzaSyDnC8LbF_TXTxanafSpT1UQBSJp5aaVWY8",
@@ -20,11 +22,5 @@ export const environment = {
     storageBucket: "pandafit-f2eb1.appspot.com",
     messagingSenderId: "995795586010",
     appId: "1:995795586010:web:9b0cfa781fd8b7981a0ee3"
-    // apiKey: "AIzaSyBm55E8-hm-5CNLN54kbAA1Nr9raqaOZaE",
-    // authDomain: "pandafit-bda26.firebaseapp.com",
-    // projectId: "pandafit-bda26",
-    // storageBucket: "pandafit-bda26.appspot.com",
-    // messagingSenderId: "91118632701",
-    // appId: "1:91118632701:web:a78f9094302cf2236e4894"
 }
 };
