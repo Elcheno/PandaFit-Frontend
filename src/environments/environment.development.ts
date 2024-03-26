dd5a27b365fb7096106dd995642a6eb613815c69
Elcheno <r.c.a2312@gmail.com>
2024-03-26 16:55:30 +0100

    RCA change environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
   dev: false,
   api: {
-    // url: 'https://pandafit-backend-0-1.onrender.com/',
-    url: 'http://localhost:8080/',
+    url: 'https://pandafit-backend-0-1.onrender.com/',
+    // url: 'http://localhost:8080/',
     institution: 'institution',
     users: '/users',
     schoolyear: '/schoolYear',
