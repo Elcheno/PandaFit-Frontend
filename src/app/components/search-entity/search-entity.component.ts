f72f0e40c22e734f0cc0efbfc5e9b881ff36f6ab
Elcheno <r.c.a2312@gmail.com>
2024-03-28 13:49:54 +0100

    RCA update search bar entity

--- src/app/components/search-entity/search-entity.component.ts
+++ src/app/components/search-entity/search-entity.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output, inject } from '@angular/core';
 import { FormBuilder, type FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subject, debounceTime } from 'rxjs';
 
 @Component({
@@ -19,6 +20,8 @@ export class SearchEntityComponent {
   private inputBufferSubject = new Subject<string>();
 
   private readonly fb = inject(FormBuilder);
+  private readonly activedRouterService = inject(ActivatedRoute);
+  private readonly routerService = inject(Router);
 
   /**
    * Form group for search input.
@@ -35,18 +38,27 @@ export class SearchEntityComponent {
     .subscribe(() => {
       this.emit();
     });
+
+    this.activedRouterService.queryParams.subscribe((res) => {
+      const search = res['search'];
+      if (search) {
+        this.form.controls['search'].setValue(search);
+        this.submit();
+      }
+    });
   }
 
   /**
    * Submits the search query.
    */
   public submit (): void {
-    //if (this.form.invalid) return;
-    // this.onSearch.emit(this.form.value.search);
     this.inputBufferSubject.next('');
   }
 
   public emit (): void {
+    this.form.value.search === '' 
+      ? this.routerService.navigate([]) 
+      : this.routerService.navigate([], { queryParams: { search: this.form.value.search } });
     this.onSearch.emit(this.form.value.search);
   }
 }
