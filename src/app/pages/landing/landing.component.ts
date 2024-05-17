22545e5cda55366ff3f3e7c46074d62c6101b765
RubenKiconex <ruben.kiconex@gmail.com>
2024-05-17 18:56:36 +0200

    update landing

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
+
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
