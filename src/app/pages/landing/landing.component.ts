9f63bf4fc74da7bdc4c8996156d27b06b16aebd4
RubenKiconex <ruben.kiconex@gmail.com>
2024-05-20 11:18:46 +0200

    update routes file and fix bugs by change routes

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ButtonComponent } from '../../components/button/button.component';
 import { AuthService } from '../../services/auth/auth.service';
-
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, RouterLink],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
