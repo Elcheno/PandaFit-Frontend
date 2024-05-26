787f7f4072e01f50f4808fd4ad0b38efa6de3ae1
najocar <josecarloscastrolopez30@gmail.com>
2024-05-26 17:11:53 +0200

    Se generan los pdf, falta implementar en algunos sitios

--- src/app/pages/formulary-dinamic-active/formulary-dynamic-active-success/formulary-dynamic-active-success.component.ts
+++ src/app/pages/formulary-dinamic-active/formulary-dynamic-active-success/formulary-dynamic-active-success.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ButtonComponent } from '../../../components/button/button.component';
+import { GeneratePdfService } from '../../../services/pdf/generate-pdf.service';
 
 @Component({
   selector: 'app-formulary-dynamic-active-success',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './formulary-dynamic-active-success.component.html',
   styleUrl: './formulary-dynamic-active-success.component.scss'
 })
 export class FormularyDynamicActiveSuccessComponent {
-
+  private pdfService = inject(GeneratePdfService);
+  private route = inject(ActivatedRoute);
   public load: boolean = false;
 
   ngOnInit(): void {
@@ -17,5 +21,25 @@ export class FormularyDynamicActiveSuccessComponent {
     }, 500);
   }
 
+  public downloadPdf(): void {
+    this.route.params.subscribe((params) => {
+      const id = params['id'];
+      this.pdfService.generatePdf(id);
+    });
+  }
+
+  extractNumberFromUrl(): void {
+    const url = this.route.snapshot.url.map(segment => segment.path).join('/');
+
+    const regex = /(\d+)$/;
+    const match = url.match(regex);
+
+    if (match) {
+      const number = match[0];
+      console.log('Número final:', number);
+    } else {
+      console.log('No se encontró un número al final de la URL');
+    }
+  }
 
 }
